f686c127b7a2cea646e52994818b71d70998d794
Mrinall Umasudhan <mrinalluma@gmail.com>
2024-10-15 11:02:58 -0400

    Update train.py

--- DonkeyModels/Mala/train/train.py
+++ DonkeyModels/Mala/train/train.py
@@ -18,16 +18,16 @@ def load_catalogs(folder: str):
     _angle = []
     _throttle = []
 
-    for _file in sorted(glob.glob(f"{folder}/*.catalog"),
+    for _file in sorted(glob.glob(f"{folder}/*.txt"),
                         key=lambda x: [
                             int(c) if c.isdigit()
                             else c for c in re.split(r'(\d+)', x)]):
         with open(_file) as f:
             for _line in f:
-                _img_name.append(_line.split()[7][1:-2])
-                _angle.append(float(_line.split()[9][0:-1]))
-                _throttle.append(float(_line.split()[13][0:-1]))
-
+                _img_name.append(_line.split()[1][:-1])
+                _angle.append(float(_line.split()[5][:-1]))
+                _throttle.append(float(_line.split()[7]))
+            
     print(f'Image count: {len(_img_name)}')
     return _img_name, _angle, _throttle
 
@@ -35,7 +35,7 @@ def load_catalogs(folder: str):
 def load_images(_img_name: list, folder: str):
     _image = []
     for i in range(len(_img_name)):
-        _img = cv2.imread(os.path.join(f"{folder}/images", _img_name[i]))
+        _img = cv2.imread(os.path.join(f"{folder}", _img_name[i]))
         assert _img.shape == (224, 224, 3),\
             "img %s has shape %r" % (_img_name[i], _img.shape)
         _image.append(_img)
@@ -47,10 +47,13 @@ def data_preprocessing(_throttle, _angle, _image):
     _steering = np.array(_angle)
     _train_img = np.array(_image)
     _label = _steering
-    _cut_height = 80
+    _cut_height = 60
     _train_img_cut_orig = _train_img[:, _cut_height:224, :]
-    _train_img_cut_gray = np.dot(_train_img_cut_orig[..., :3],
-                                 [0.299, 0.587, 0.114])
+    _train_img_cut_gray = []
+    for image in _train_img_cut_orig: 
+        grey_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _train_img_cut_gray.append(grey_img)
+    _train_img_cut_gray = np.array(_train_img_cut_gray)
     return _train_img_cut_orig, _train_img_cut_gray, _label
 
 
@@ -96,7 +99,7 @@ def train_start(_model, _X_train, _X_test, _y_train, _y_test):
     _model.compile(optimizer=_optimizer, loss="mse", metrics=["accuracy"])
     _model.summary()
     _trained_model = _model.fit(_X_train, _y_train,
-                                epochs=3000, batch_size=20,
+                                epochs=200, batch_size=20,
                                 validation_data=(_X_test, _y_test))
     return _trained_model
 
@@ -131,15 +134,19 @@ def plot_trained_model(_trained_model, show: bool = False,
 
 
 if __name__ == "__main__":
-    data_folder = "Your Data"
+    data_folder = "/home/mrinall/TEA/DonkeyCar-TEA/DonkeyModels/Mala/train/my_data/"
     img_name, angle, throttle = load_catalogs(data_folder)
     image = load_images(img_name, data_folder)
     image = np.array(image)
     train_img_cut_orig, train_img_cut_gray, label = data_preprocessing(
         throttle, angle, image)
+    # print(f"Image Input Shape: {train_img_cut_gray.shape}")
+    # cv2.imshow("window", train_img_cut_gray[0])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     X_train, X_test, y_train, y_test = train_split(
         train_img_cut_orig, train_img_cut_gray, label)
-    model = default_n_linear(1, input_shape=(144, 224))
+    model = default_n_linear(1, input_shape=(164, 224))
     trained_model = train_start(model, X_train, X_test, y_train, y_test)
-    plot_trained_model(trained_model, show=False, save=True)
     model.save(f"model_{time.ctime(time.time())}.h5")
+    plot_trained_model(trained_model, show=False, save=False)
